e1f3065c5ca7cc6a68c87cf90d43a30ddbcbb5e5
tothagnes95 <tothagnes95@gmail.com>
2023-09-20 10:12:49 +0200

    project created

---
+++ Travel_Log/src/main/java/com/example/travel_log/TravelLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelLogApplication.class, args);
+	}
+
+}
---
+++ Travel_Log/src/test/java/com/example/travel_log/TravelLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
